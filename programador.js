46e1128fbbb2dcf625bcecea9eaf910eba941ef6
Magnopach <magnopacheco67@gmail.com>
2020-06-16 18:26:06 -0300

    programador commit

---
+++ programador.js
@@ -0,0 +1,37 @@
+
+const programador = {
+    nome: "Magno",
+    idade: 25,
+    tecnologias: [
+        { nome: 'C++',
+        especialidade: 'Desktop'
+        },
+        { nome: 'Python',
+        especialidade: 'Data Science' 
+        },
+        { nome: 'JavaScript',
+        especialidade: 'Web/Mobile' 
+        }
+    ] 
+}
+
+console.log(`O usuário ${programador.nome} tem ${programador.idade} anos e usa a tecnologia ${programador.tecnologias[0].nome} com especialidade em ${programador.tecnologias[0].especialidade} `)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
